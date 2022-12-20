989efc1e4d145a629e70d03ba03b172e72939f30
sianiryi <sian07080@gmail.com>
2022-12-20 16:52:24 +0900

    add force
    마우스 클릭시 바람 추가

--- sketch.js
+++ sketch.js
@@ -79,6 +79,11 @@ function setup() {
 function draw() {
   background(0);
 
+  if(mouseIsPressed){
+    let wind = createVector(0.1,0);
+   attractor.applyForce(wind);
+    }
+
   attractor.display();
 
   for (let i = 0; i < movers.length; i++) {
