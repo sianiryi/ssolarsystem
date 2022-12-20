185898ee663271fcb26588f9b524fe10cfe59b38
sianiryi <sian07080@gmail.com>
2022-12-20 17:11:48 +0900

    edge 추가
    캔버스 끝에 닿으면 (?,0) 부터 다시 시작하게 만들었다.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,4 @@
-// The Nature of Code
-// Daniel Shiffman
-// http://natureofcode.com
 let movers = [];
-
-
-
 
 let earth;
 let sun;
@@ -16,31 +10,17 @@ let mars;
 let neptune;
 let uranus;
 
-
-
 function preload() {
-  sun = loadImage ('sun.png');
-  earth = loadImage ('pngegg.png');
-  mercury = loadImage ('mercury.png');
-  venus = loadImage ('venus.png');
-  jupiter = loadImage ('jupiter.png');
-  saturn = loadImage ('saturn.png');
-  mars = loadImage ('mars.png');
-  neptune = loadImage ('neptune.png');
-  uranus = loadImage ('uranus.png');
-
+  sun = loadImage("sun.png");
+  earth = loadImage("pngegg.png");
+  mercury = loadImage("mercury.png");
+  venus = loadImage("venus.png");
+  jupiter = loadImage("jupiter.png");
+  saturn = loadImage("saturn.png");
+  mars = loadImage("mars.png");
+  neptune = loadImage("neptune.png");
+  uranus = loadImage("uranus.png");
 }
-
-// let sun;
-// function preload() {
-//   sun = loadImage ('sun.png');
-// }
-
-// let earth;
-// function preload() {
-//   earth = loadImage ('earth.png');
-// }
-
 
 let attractor;
 
@@ -55,34 +35,34 @@ function setup() {
   for (let i = 0; i < 6; i++) {
     movers[i] = new Mover3(random(0.1, 2), random(width), random(height));
   }
-   for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     movers[i] = new Mover4(random(0.1, 2), random(width), random(height));
   }
-   for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
     movers[i] = new Mover5(random(0.1, 2), random(width), random(height));
   }
-   for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
     movers[i] = new Mover6(random(0.1, 2), random(width), random(height));
   }
-    for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 2; i++) {
     movers[i] = new Mover7(random(0.1, 2), random(width), random(height));
   }
-    for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 1; i++) {
     movers[i] = new Mover8(random(0.1, 2), random(width), random(height));
   }
 
   attractor = new Attractor();
-  
-
 }
 
 function draw() {
   background(0);
 
-  if(mouseIsPressed){
-    let wind = createVector(1,1);
-   attractor.applyForce(wind);
-    }
+  if (mouseIsPressed) {
+    let wind = createVector(1, 1);
+    attractor.applyForce(wind);
+  }
+
+  attractor.edges();
 
   attractor.display();
 
